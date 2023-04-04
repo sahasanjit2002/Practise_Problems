d5c8353bcbedb719ebab704e686654f96cb0d13b
Sanjit Saha <57536368+sahasanjit2002@users.noreply.github.com>
2023-04-04 22:23:13 +0530

    Heap Sort Update

--- src/HeapSort.java
+++ src/HeapSort.java
@@ -17,36 +17,16 @@ public class HeapSort {
 
     public static void heapify(int arr[], int N, int i)
     {
-        // Find largest among root, left child and right child
-    
-        // Initialize largest as root
+
         int largest = i;
-    
-        // left = 2*i + 1
         int left = 2 * i + 1;
-    
-        // right = 2*i + 2
         int right = 2 * i + 2;
-    
-        // If left child is larger than root
         if (left < N && arr[left] > arr[largest])
-    
             largest = left;
-    
-        // If right child is larger than largest
-        // so far
         if (right < N && arr[right] > arr[largest])
-    
             largest = right;
-    
-        // Swap and continue heapifying if root is not largest
-        // If largest is not root
         if (largest != i) {
-    
             swap(arr, i, largest);
-    
-            // Recursively heapify the affected
-            // sub-tree
             heapify(arr, N, largest);
         }
     }
@@ -54,18 +34,13 @@ public class HeapSort {
     public static void heapSort(int arr[])
     {
         int N = arr.length;
-        // Build max heap
         for (int i = N / 2 - 1; i >= 0; i--)
     
             heapify(arr, N, i);
     
         // Heap sort
         for (int i = N - 1; i >= 0; i--) {
-    
             swap(arr, 0, i);
-    
-            // Heapify root element to get highest element at
-            // root again
             heapify(arr, i, 0);
         }
     }
